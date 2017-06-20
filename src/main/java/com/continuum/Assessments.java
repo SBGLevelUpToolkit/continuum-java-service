72929e9bcfc1fc803dc882d4f67d3b3ee37fafef
Brain Leke <brian@rfc.private.address.invalid.query>
2017-06-20 16:35:33 +0200

    Brain: Adding more unit tests

--- src/main/java/com/continuum/Assessments.java
+++ src/main/java/com/continuum/Assessments.java
@@ -12,4 +12,24 @@ public class Assessments {
         this.portfolio = portfolio;
         this.assessments = assessments;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Assessments that = (Assessments) o;
+
+        if (dateAssessed != null ? !dateAssessed.equals(that.dateAssessed) : that.dateAssessed != null) return false;
+        if (portfolio != null ? !portfolio.equals(that.portfolio) : that.portfolio != null) return false;
+        return assessments != null ? assessments.equals(that.assessments) : that.assessments == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = dateAssessed != null ? dateAssessed.hashCode() : 0;
+        result = 31 * result + (portfolio != null ? portfolio.hashCode() : 0);
+        result = 31 * result + (assessments != null ? assessments.hashCode() : 0);
+        return result;
+    }
 }
---
+++ src/test/java/AssessmentsTest.java
@@ -0,0 +1,73 @@
+import com.continuum.Assessment;
+import com.continuum.Assessments;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+public class AssessmentsTest {
+
+    Assessments assessments;
+    String dateAssessed = "testDate";
+    String portfolioName = "portfolioName";
+    ArrayList<Assessment> assessmentArray;
+
+    @Before
+    public void setUp(){
+        assessmentArray = new ArrayList<Assessment>();
+        assessments = new Assessments(dateAssessed, portfolioName, assessmentArray);
+    }
+
+    @Test
+    public void shouldKnowHowToDefineAssessments(){
+        assertEquals(assessments.getClass(), Assessments.class);
+    }
+
+    @Test
+    public void shouldKnowTwoAssessmentsWithSameValuesAreEqual(){
+        Assessments anotherAssessments = new Assessments(dateAssessed, portfolioName, assessmentArray);
+        assertThat(assessments, is(equalTo(anotherAssessments)));
+    }
+
+    @Test
+    public void shouldKnowTwoAssessmentsWithDifferentDateAssessedValuesAreEqual(){
+        String anotherDateAssessed = "anotherTestDate";
+        Assessments anotherAssessments = new Assessments(anotherDateAssessed, portfolioName, assessmentArray);
+        assertThat(assessments, is(not(equalTo(anotherAssessments))));
+    }
+
+    @Test
+    public void shouldKnowTwoAssessmentsWithDifferentPortfolioNameValuesAreEqual(){
+        String anotherPortfolioName = "anotherTestDate";
+        Assessments anotherAssessments = new Assessments(dateAssessed, anotherPortfolioName, assessmentArray);
+        assertThat(assessments, is(not(equalTo(anotherAssessments))));
+    }
+
+    @Test
+    public void shouldKnowTwoAssessmentsWithDifferentAssessmentValuesAreEqual(){
+        ArrayList<Assessment> anotherAssessmentsArray = new ArrayList<Assessment>();
+        anotherAssessmentsArray.add(new Assessment());
+        Assessments anotherAssessments = new Assessments(dateAssessed, portfolioName, anotherAssessmentsArray);
+        assertThat(assessments, is(not(equalTo(anotherAssessments))));
+    }
+
+    @Test
+    public void shouldKnowTwoAssessmentsWithSameAssessmentValuesAreEqual(){
+        ArrayList<Assessment> anotherAssessmentsArray = new ArrayList<Assessment>();
+        Assessment assessment = new Assessment();
+
+        assessmentArray.add(assessment);
+        anotherAssessmentsArray.add(assessment);
+
+        assessments = new Assessments(dateAssessed, portfolioName, assessmentArray);
+        Assessments anotherAssessments = new Assessments(dateAssessed, portfolioName, anotherAssessmentsArray);
+
+        assertThat(assessments, is(equalTo(anotherAssessments)));
+    }
+}
